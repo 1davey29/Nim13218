38ff64e48c959a255a01f3311346ce52e99b6d2d
lukeshep42 <lukeshep42@hotmail.com>
2018-10-18 06:26:33 -0600

    Added UnitTest project

---
+++ Nim/NimTests1/GameWindowTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nim;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nim.Tests
+{
+    [TestClass()]
+    public class GameWindowTests
+    {
+        [TestMethod()]
+        public void SetupTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
